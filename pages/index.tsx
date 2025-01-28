fc7bbfb3211f4e1dbe9ee4fefe5998c526f0c118
evanhadam <hadame.eh@gmail.com>
2025-01-28 00:39:22 -0500

    CSS for body text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import styles from '../styles/Home.module.css';
 import HomeHeader from '../components/HomeHeader';
 import ExperienceCard from '../components/ExperienceCard';
 import CenteredContainer from '../components/CenteredContainer';
 import SiteHead from '@/components/SiteHead';
+import Spacer from '@/components/Spacer';
 
 export default function Home() {
   return (
@@ -13,26 +15,27 @@ export default function Home() {
 
       <HomeHeader />
 
+      <Spacer bottom={100} />
+
       <div id="about">
         <CenteredContainer>
-          <p>
+          <p className={styles.contentText}>
             Hey! I'm Evan Hadam, a CS + Econ student at the University of Florida.
           </p>
-          <p>
+          <p className={styles.contentText}>
             I spent much of college not planning on going into tech. I wanted to make a difference through direct patient care 
             as a doctor before working as a programmer in medical research labs around campus shifted my interests.
           </p>
-          <p>
+          <p className={styles.contentText}>
             This change led me to working internships at a medical device company and an electronic health record company
             to hone my tech skills in a familiar environment. 
           </p>
-          <p>
+          <p className={styles.contentText}>
             Now, I'm building software projects used by healthcare professionals across Florida and serving as a leader for a 
             student-run MedTech engineering team at UF. 
           </p>
-          <p>
-            It's hard to know what the next few years are going to look like, but the journey has me ready to take whatever 
-            jobs, opportunities, cities, or industries come my way and run with them.
+          <p className={styles.contentText}>
+            These past few years have been really exciting and I'm excited to find new ways to solve people's problems.
           </p>
         </CenteredContainer>
       </div>
