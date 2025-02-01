1bf8b3581f4337fcb9ab8c806efecc8e48cf28f2
evanhadam <hadame.eh@gmail.com>
2025-01-31 22:44:01 -0500

    Nightly commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,3 +18,14 @@ interface footerProps {
     columnOptions: footerColumn[];
     socialOptions: socialIcon[];
 }
+
+export default function Footer (props: footerProps)
+{
+    return (
+        <footer className="bg-gray-800 pt-10 mt-10 pt-10">
+            <div className="max-w-6xl text-gray-800">
+
+            </div>
+        </footer>
+    )
+}
